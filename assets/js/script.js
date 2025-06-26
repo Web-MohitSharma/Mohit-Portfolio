578caceca858e54af28e66cb488233f02ad94c48
Web-MohitSharma <skmohit9955@gmail.com>
2025-06-26 22:33:55 +0530

    Debugging bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,9 +168,7 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 
 // disable developer mode
 document.onkeydown = function (e) {
-    if (e.keyCode == 123) {
-        return false;
-    }
+
     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
         return false;
     }
